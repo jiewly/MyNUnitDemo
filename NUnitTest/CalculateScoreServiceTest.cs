5337b9182a3be3cfd9dab8b1220d10ba17a77cc4
Irin Krapookthong (NTL) <CF303520@tidlor.com>
2021-09-10 16:16:37 +0700

    init

---
+++ NUnitTest/CalculateScoreServiceTest.cs
@@ -0,0 +1,86 @@
+﻿using MyNUnitDemo.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest
+{
+    class CalculateScoreServiceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        //given-when-then
+        [Test]
+        public void Score73_Execute_True()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("73");
+
+            //check unit
+            Assert.True(res);
+        }
+
+        [Test]
+        public void Score50_Execute_True()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("50");
+
+            //check unit
+            Assert.True(res);
+        }
+        [Test]
+        public void Score21_Execute_False()
+        {
+            var calc = new CalculateScoreService();
+            var res = calc.Execute("21");
+
+            //check unit
+            Assert.False(res);
+        }
+        [Test]
+        public void ScoreXXX_Execute_TrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute("XXX"));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+        [Test]
+        public void ScoreNull_Execute_TrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute(null));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+        [Test]
+        public void Score500point25_Execute_TrowException()
+        {
+            var expected = "Error! Please input number.";
+            var calc = new CalculateScoreService();
+            //var res = calc.Execute("xxx");
+            var res = Assert.Throws<Exception>(() => calc.Execute("500.25"));
+
+            //check unit
+            Assert.AreEqual(expected, res.Message);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ NUnitTest/LicenseRenewServiceTest.cs
@@ -0,0 +1,71 @@
+﻿using Autofac.Extras.Moq;
+using Moq;
+using MyNUnitDemo.Models;
+using MyNUnitDemo.Repositories;
+using MyNUnitDemo.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnitTest
+{
+    public class LicenseRenewServiceTest
+    {
+        private AutoMock autoMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            autoMock = AutoMock.GetLoose();
+        }
+
+        [Test]
+        public void GetAll_HaveModels_ReturnListModel()
+        {
+            autoMock.Mock<ILicenseRenewRepository>().Setup(m => m.GetAll()).Returns(GetLicenseRenews());
+            var licenseRenewsService = autoMock.Create<LicenseRenewService>();
+            var licenseRenews = licenseRenewsService.GetAll().ToString();
+            Assert.Greater(licenseRenews.Count(), 0);
+        }
+
+        [Test]
+        public void GetById_Found_ReturnModel()
+        {
+            autoMock.Mock<ILicenseRenewRepository>().Setup(m => m.GetById(It.IsAny<string>())).Returns((string licenseId) => GetLicenseRenews().Where(m => m.FK_LicenseId == licenseId).FirstOrDefault());
+
+            var licenseRenewService = autoMock.Create<LicenseRenewService>();
+            var res = licenseRenewService.GetById("100");
+            Assert.Null(res);
+            
+        }
+
+        [Test]
+        public void GetById_NotFound_ReturnNull()
+        {
+            autoMock.Mock<ILicenseRenewRepository>().Setup(m => m.GetById(It.IsAny<string>())).Returns((string licenseId) => GetLicenseRenews().Where(m => m.FK_LicenseId == licenseId).FirstOrDefault());
+
+            var licenseRenewService = autoMock.Create<LicenseRenewService>();
+            var res = licenseRenewService.GetById("11");
+            Assert.NotNull(res);
+            Assert.AreEqual(res.FK_PONo, "A002");
+        }
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        public IEnumerable<LicenseRenew> GetLicenseRenews()
+        {
+            return new List<LicenseRenew>{
+                new LicenseRenew{ Id = 1, FK_PONo="A001", FK_LicenseId="10"},
+                new LicenseRenew{ Id = 2, FK_PONo="A002", FK_LicenseId="11"}
+                };
+        }
+    }
+}
